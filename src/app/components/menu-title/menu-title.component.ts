4b9e68dd53b14fb69d5a6c9e25fc51c0cb046f9d
letlm <lealmleticia@gmail.com>
2023-08-29 19:47:46 -0300

    chore: big card, small card e menu-title responsivos

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css']
+  styleUrls: ['./menu-title.component.css'],
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
